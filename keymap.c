e72304ffcd70e6823a3f03de551337a7fc398ae5
xelvious <ohtake@tle.tokyo>
2018-11-10 10:00:27 +0900

    adjust my favorite

--- keymap.c
+++ keymap.c
@@ -33,14 +33,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |  , < |  . > |  / ? | Fn   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      | Alt  | OS   | Sp/Lw| En/Rs| OS   | Left | Down | Up   | Right|
+ * |      |      |      | Alt  | OS   | Sp/Lw| En/Rs| Alt  | Left | Down | Up   | Right|
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_ortho_4x12(
    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,      KC_BSPC, \
    KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,   KC_QUOT, \
    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,   MO(_FN), \
-   _______, _______, _______, KC_LALT, KC_LGUI, LT(_LOWER,KC_SPC), LT(_RAISE,KC_ENT), KC_RGUI,KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT  \
+   _______, _______, _______, KC_LALT, KC_LGUI, LT(_LOWER,KC_SPC), LT(_RAISE,KC_ENT), KC_RALT,KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT  \
 ),
 
 /* Lower
